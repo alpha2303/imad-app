59e63da66e2b31fae8ed61cdabf95c16743f9043
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-08-09 21:46:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,69 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : "Rahul's Article V1.0",
+    heading : "Article One",
+    date : "Aug 9th, 2016",
+    content : 
+    `
+     <p>
+          ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.
+     </p>
+     <p>  
+          ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.
+     </p>
+     <p>
+          ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.ThIs is my first HTML File.
+     </p>
+     `
+     
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate =
+    `
+     <head>
+            <title>
+              ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href = "/ui/style.css" rel="stylesheet">
+        </head>
+    <body>
+        <div class="container">
+         <div>
+         <a href = "/">Home</a>
+         </div>
+         <hr/>
+         <h3>
+             <marquee>
+                ${heading}
+             </marquee>
+         </h3>
+         <div>
+             ${date}
+         </div>
+         ${content}
+        </div>
+    </body
+    </html>
+    `;
+    
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+    res.send(createTemplate(articleOne))); 
 });
 
 app.get('/article-two', function (req, res){
