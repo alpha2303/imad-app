c6b873190a0b50e231b70a4bc00a3cbf72341f38
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-08-14 23:08:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,12 @@ element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
 
+var marginLeft = 0;
 
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 
 img.onclick = function () {
     img.style.marginLeft = '200px';
