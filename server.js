2dfb868c0f1ed380b420d0c55bccec36c28b4302
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-08-16 19:51:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,18 @@ app.get('/counter', function (req,res){
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res){
+    
+   // Get the name from the request 
+   var name = req.query.name;
+   names.push(name);
+   
+   //JSON : Javascript Object Notation
+   
+   res.send(JSON.stringify(names));
+   
+});
 app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
