5bb8a74f5d315b2e97ee224338938d8237713dd9
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-08-16 19:41:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,11 @@ var button = document.getElementById('counter');
 button.onclick = function (){
     
     // Create a request object
+    
     var request = new XMLHttpRequest();
     
     // Capture the response and store it in variable.
+    
     request.onreadystatechange = function () {
        if(request.readyState === XMLHttpRequest.DONE){
            // Take some action
@@ -21,4 +23,25 @@ button.onclick = function (){
     
     request.open('GET','http://rahulpavithran99.imad.hasura-app.io/counter',true);
     request.send(null);
+};
+
+//Name Submit
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+  // Make a request to the server and send a name
+  
+  
+  // Capture the list of names and render it as a list
+  
+  var names = ["name1" , "name2" , "name3"];
+  var list = '';
+  for(var i=0;i<names.length;i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
 };
