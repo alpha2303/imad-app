13bdfa03348edfe6567ba8c3100d4301a643fbbd
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-08-26 16:59:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,12 +75,39 @@ app.post('/create-user', function (req, res) {
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbstring = hash(password, salt);
-    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbstring], function(err, result) {
+    pool.query('INSERT INTO "user" WHERE (username, password) VALUES ($1, $2)', [username, dbstring], function(err, result) {
         if(err) {
           res.status(500).send(err.toString());
       }
       else {
           res.send('User successfully created : ' + username);
+      }
+    });
+});
+
+app.post('/login', function (req,res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" username =$1', [username], function(err, result) {
+        if(err) {
+          res.status(500).send(err.toString());
+        }
+        else {
+          if(result.rows.length === 0){
+              res.send(403).send('username/password is invalid!');
+          }
+          else{
+          //Match password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt);
+              if(hashedPassword === dbString){
+                  res.send('Credentials are Correct');
+              }
+              else{
+                  res.send(403).send('username/password is invalid!');
+              }
+         }
       }
     });
 });
