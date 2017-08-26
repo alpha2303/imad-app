81c3956bb36cd5616f9113ac793bd692ce980e87
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-08-26 18:42:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,5 @@
-// Counter code
-var button = document.getElementById('counter');
-button.onclick = function (){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://rahulpavithran99.imad.hasura-app.io/counter', true);
-    request.send(null);
-  
-};
 
-//Name Submit
-
+//Login Credential submission
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
@@ -32,26 +14,23 @@ submit.onclick = function () {
        if(request.readyState === XMLHttpRequest.DONE){
            // Take some action
            if(request.status === 200){
-              // Capture the list of names and render it as a list
-                    var names = request.responseText;
-                    names = JSON.parse(names);
-                  var list = '';
-                  for(var i=0;i<names.length;i++){
-                      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+             console.log("Credentials correct");
+             alert("User logged in successfully");
            }
-    }
-    
+           else if (request.readyState === 403){
+               alert("Username/password is incorrect!");
+           }
+           else if (request.readyState === 500){
+               alert("Something went wrong with the server! We're trying to fix it.... ")
+           }
+       }
     };
     // Make a request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://rahulpavithran99.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
-    
-  // Make a request to the server and send a name
-    
-  
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://rahulpavithran99.imad.hasura-app.io/login' + name,true);
+    request.setRequestHeader("Content-type","application/json");
+    request.send(JSON.stringify({username: username, password: password}));
 };
