29b020c66e8d91238fc52d5dd5c1a17e57cb60c7
alpha2303 <30804672+alpha2303@users.noreply.github.com>
2017-09-17 13:18:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -179,9 +179,9 @@ app.get('/submit-name', function (req, res){
    
 });
 
-app.get('/articles/:articleName', function (req, res){
+app.get('/get-articles', function (req, res){
    
-   pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName], function (err,result) {
+   pool.query("SELECT * FROM articles", function (err,result) {
       if(err){
           res.status(500).send(err.toString());
       }
